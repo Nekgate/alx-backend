750eb0b306042ab97f78b505c572e03e12799926
Nekgate <Nekgate@gmail.com>
2024-07-23 20:17:40 +0100

    LIFO Caching

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -11,22 +11,17 @@ class LIFOCache(BaseCaching):
     def __init__(self):
         """ Initialise Class instance. """
         super().__init__()
-        self.cache_keys = []
 
     def put(self, key, item):
         """ Add an item in cache. """
-        if key is None or item is None:
-            return
-        #  Check if cache is full.
-        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            last_key = self.cache_keys.pop(0)  # Discard the last item (LIFO)
-            del self.cache_data[last_key]
-            print(f'DISCARD: {last_key}')
-        self.cache_keys.append(key)
-        self.cache_data[key] = item  # Assign item to key in cache_data.
+        if key and item:
+            self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            self.cache_data.pop(self.last_item)
+            print(f'DISCARD: {self.last_item}')
+        if key:
+            self.last_item = key
 
     def get(self, key):
         """ Get an item by key. """
-        if key is None or key not in self.cache_data:
-            return None
         return self.cache_data.get(key)
